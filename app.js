bcfae3a342a893608144c5710b0347eb1b2abb76
Daniyal Faiz <daniyalfaiz@vmi2208320.contaboserver.net>
2024-11-10 22:02:38 -0600

    Add Node.js app and GitHub Actions workflow

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello World Workflow!');
+  res.send('Hello World Workflow is Ready!');
 });
 app.listen(5000, function () {
   console.log('Example app listening on port 5000!');
